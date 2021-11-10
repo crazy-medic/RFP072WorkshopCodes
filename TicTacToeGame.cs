76e64867c65b76ed22b88de0ad32ff0b96b26954
Vineeth <35455584+crazy-medic@users.noreply.github.com>
2021-11-10 12:11:57 +0530

    Update TicTacToeGame.cs

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -11,7 +11,7 @@ namespace WorkshopTicTacProgram
 
         public void DrawBoard()
         {
-            for(int i = 0; i < 10; i++)
+            for(int i = 1; i < 10; i++)
             {
                 Gamegrid[i] = V;
             }
